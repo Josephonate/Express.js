be0ecbe15f47b45d243d6f591e1d140709c12c58
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-09-20 00:25:23 -0400

    added routes to public folder

--- expres.js
+++ expres.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
-app.get('/api/notes')
 
 app.listen(PORT, () => {
     console.log(`server available at https://localhost:${PORT}`);
--- public/routes/api.js
+++ public/routes/api.js
@@ -1,10 +1,15 @@
-const express = require ('express');
+const express = require('express');
 const path = require('path');
-const fs = require ('fs');
+const fs = require('fs');
 
 
-const uuid = require ('../helpers/uuid');
+const uuid = require('../helpers/uuid');
 const app = express();
 
+app.get('/api/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.get
+app.post('/api/notes', (req, res) =>
+    res.sendfile(path.join(__dirname, '/public/pages/notes.html'))
+);
